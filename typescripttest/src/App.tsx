04d064ba12043d5ad733418ef1a3cde652dd5a45
Adam Smasher <iliesamineizikki@gmail.com>
2024-11-29 13:32:39 +0100

    bataille

--- typescripttest/src/App.tsx
+++ typescripttest/src/App.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import BlackJack from './components/BlackJack';
+import Bataille from './components/Bataille';
 
 function App() {
   return (
     <div className="App">
-        <p>
-          TypeScript Test
-        </p>
-        <BlackJack />
+        <Bataille />
     </div>
   );
 }
